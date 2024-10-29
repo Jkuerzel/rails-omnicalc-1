855885955b9222f1d049a0d75a9ad38fd6c583df
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-10-29 02:53:50 +0000

    First Commit

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,40 @@
+class ZebraController < ApplicationController
+
+  def home
+    render({:template => "calc_templates/square"})
+  end
+
+  def square
+    render({:template => "calc_templates/square"})
+  end
+
+  def square_result
+  
+  end
+
+  def square_root
+
+  end
+
+  def square_root_result
+  
+  end
+
+  def payment
+  
+  end
+
+  def payment_result
+  
+  end
+
+  def random
+
+  end
+
+  def random_result
+  
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get("/", {:controller=>"zebra", :action=>"add"})
-  get("/square/new", {:controller=>"zebra", :action=>"home"})
-  get("/square_root/new", {:controller=>"zebra", :action=>"home"})
-  get("/payment/new", {:controller=>"zebra", :action=>"home"})
-  get("/random/new", {:controller=>"zebra", :action=>"home"})
+  get("/", {:controller=>"zebra", :action=>"home"})
+  
+  get("/square/new", {:controller=>"zebra", :action=>"square"})
+  get("/square/results", {:controller=>"zebra", :action=>"square_result"})
+  
+  get("/square_root/new", {:controller=>"zebra", :action=>"square_root"})
+  get("/square_root/results", {:controller=>"zebra", :action=>"square_root_result"})
+  
+  get("/payment/new", {:controller=>"zebra", :action=>"payment"})
+  get("/payment/results", {:controller=>"zebra", :action=>"payment_result"})
 
+  get("/random/new", {:controller=>"zebra", :action=>"random"})
+  get("/random/results", {:controller=>"zebra", :action=>"random_result"})
 
 end
