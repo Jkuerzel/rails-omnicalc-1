697ef6b20bf1cec18aca7de73a3e7f0c87194715
Jkuerzel <jkuerzel@chicagobooth.edu>
2024-10-29 02:32:21 +0000

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller=>"zebra", :action=>"add"})
+  get("/square/new", {:controller=>"zebra", :action=>"home"})
+  get("/square_root/new", {:controller=>"zebra", :action=>"home"})
+  get("/payment/new", {:controller=>"zebra", :action=>"home"})
+  get("/random/new", {:controller=>"zebra", :action=>"home"})
+
+
 end
